fa3648299d19400ef0ab0c976e87db638a276cc5
tarunsharma1998 <rtdigitalsolution@gmail.com>
2020-05-19 18:05:59 +0530

    Fateh changes_20200519

---
+++ AccountvalidationWebAPI/Models/AccountValidationModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AccountvalidationWebAPI.Models
+{
+    public class LoginResponse
+    {
+        //public List<Mandate> MandateData
+        //{ get; set; }
+        public string MdtID { get; set; }
+        //public string IsliveInNach { get; set; }
+        public string Message { get; set; }
+        public string Status { get; set; }
+        public string AccountHolderName { get; set; }
+        public string ResCode { get; set; }
+        public string MandateData { get; set; }
+    }
+    public class AccountValidationModel
+    {
+        public string MandateId { get; set; }
+        //public string IsliveInNach { get; set; }
+        public string ActivityId { get; set; }
+        public string AcNo { get; set; }
+        public string IFSC { get; set; }
+        public string UserId { get; set; }
+        public string AppId { get; set; }
+        public string EntityId { get; set; }
+    }
+    public class InsertrequestModel
+    {
+        public string Customer1 { get; set; }
+        //public string IsliveInNach { get; set; }
+        public string LengthName { get; set; }
+        public Int64 paymentRequestId { get; set; }
+       
+    }
+    public class InsertrequestNOModel
+    {
+        public string RequestNo { get; set; }      
+    }
+}
---
+++ BankValidationWebAPI/Controllers/BankValidationController.cs
@@ -0,0 +1,31 @@
+﻿using BankValidationWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BankValidationWebAPI.Controllers
+{
+    public class BankValidationController : ApiController
+    {
+        [HttpPost]
+        [Route("api/BankValidation/BankValidation")]
+        public bankValidationResponseData UpdateFirst(BankValidationModal MData)
+        {
+            try
+            {
+                return BankValidationMethods.bankvalida(MData);
+
+               // ds.Tables[4].Rows[0]["mandateid"] = DbSecurity.Encrypt(Convert.ToString(ds.Tables[4].Rows[0]["mandateid"]));
+            }
+            catch
+            {
+                return null;
+            }
+            
+
+        }
+    }
+}
