80610cd2419306329a42e65f674affd2a44a5807
vimalakgec <45147202+vimalakgec@users.noreply.github.com>
2020-05-21 15:21:54 +0530

    vimal

---
+++ SaveEditMandateAPI/Models/BankForm/BindIFSC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaveEditMandateAPI.Models.BankForm
+{
+    public class BindIFSC
+    {
+        public string BankName { get; set; }
+        public string IFSC { get; set; }
+    }
+}
