9a0b8afad773da89d34f68f76aa628f87b983222
Avinash Gupta <avinash1@yoekisoft.com>
2020-05-25 20:15:04 +0530

    Commit

---
+++ SetupAPI/Controllers/BusinessLogics/LinkSetupController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SetupAPI.Models;
+using BusinessLibrary;
+using EntityDAL;
+using Encryptions;
+
+namespace SetupAPI.Controllers.BusinessLogics
+{
+    public class LinkSetupController : ApiController
+    {
+        QuickCheck_AngularEntities dbcontext = new QuickCheck_AngularEntities();
+
+        [HttpPost]
+        [Route("api/Accountvalidation/Accountvalidation")]
+        public void GetList()
+        {
+            try
+            {
+                var Result = dbcontext.MultipleResults("[dbo].[Sp_UserLogin]").With<LinkSetupModel>().Execute("@QueryType", "GetLinks");
+            }
+            catch (Exception ex)
+            { }
+
+        }
+    }
+}
---
+++ SetupAPI/Models/LinkSetupModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SetupAPI.Models
+{
+    public class LinkSetupModel
+    {
+        public string LinkID { get; set; }
+        public string LinkName { get; set; }
+        public string IconName { get; set; }
+        public string Purpose { get; set; }
+        public string url { get; set; }
+        public string IsActive { get; set; }
+        public string ParentId { get; set; }
+        public string OrderNo { get; set; }
+    }
+}
