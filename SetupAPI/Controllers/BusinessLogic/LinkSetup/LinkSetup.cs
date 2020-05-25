dc7beed603e5c95ffa78d10874d1a8f8df7e673a
Avinash Gupta <avinash1@yoekisoft.com>
2020-05-25 15:24:37 +0530

    Link Setup

---
+++ SetupAPI/Controllers/BusinessLogic/LinkSetup/LinkSetup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SetupAPI.Controllers.BusinessLogic.LinkSetup
+{
+    public class LinkSetup
+    {
+    }
+}
