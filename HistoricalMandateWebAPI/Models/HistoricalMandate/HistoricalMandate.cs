f47f44e72dd19f944ae530d428fc0f20a389de57
vimalakgec <45147202+vimalakgec@users.noreply.github.com>
2020-05-26 14:48:11 +0530

    space changes

--- HistoricalMandateWebAPI/Models/HistoricalMandate/HistoricalMandate.cs
+++ HistoricalMandateWebAPI/Models/HistoricalMandate/HistoricalMandate.cs
@@ -43,7 +43,6 @@ namespace HistoricalMandateWebAPI.Models.HistoricalMandate
 
                 return res;
             }
-
             catch (Exception ex)
             {
                 throw ex;
